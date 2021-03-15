a1d5dc08500735528ed18620e8d167b554e53da6
QianRuan <klararuan@gmail.com>
2021-03-15 22:51:47 +0100

    Add script for pyrouge setup

---
+++ setup_pyrouge.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/bheinzerling/pyrouge
+cd pyrouge
+pip install -e .
+git clone https://github.com/andersjo/pyrouge.git rouge
+pyrouge_set_rouge_path ~/pyrouge/rouge/tools/ROUGE-1.5.5/
