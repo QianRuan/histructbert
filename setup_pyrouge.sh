f72e1297bcab6f41f1db4493df0210cc6c3af7e1
QianRuan <klararuan@gmail.com>
2021-03-26 23:39:21 +0100

    Add files via upload

--- setup_pyrouge.sh
+++ setup_pyrouge.sh
@@ -4,8 +4,6 @@ pip install -e .
 git clone https://github.com/andersjo/pyrouge.git rouge
 pyrouge_set_rouge_path pyrouge/rouge/tools/ROUGE-1.5.5/
 #########################
-conda install -c bioconda perl-xml-libxml
-#########################
 cd pyrouge/rouge/tools/ROUGE-1.5.5/data
 rm WordNet-2.0.exc.db # only if exist
 cd WordNet-2.0-Exceptions
@@ -14,3 +12,6 @@ rm WordNet-2.0.exc.db # only if exist
 ./buildExeptionDB.pl . exc WordNet-2.0.exc.db
 cd ../
 ln -s WordNet-2.0-Exceptions/WordNet-2.0.exc.db WordNet-2.0.exc.db
+#########################
+conda install -c anaconda perl
+conda install -c bioconda perl-xml-libxml
