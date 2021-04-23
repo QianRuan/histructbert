449790a5a88a60fe089d329478f7c59796fc9780
QianRuan <klararuan@gmail.com>
2021-04-23 16:16:38 +0200

    update

--- src/train.py
+++ src/train.py
@@ -43,6 +43,7 @@ if __name__ == '__main__':
     parser.add_argument('-log_file', default='')
     parser.add_argument("-result_path", default='')
     parser.add_argument("-select_top_n_sent", default=3, type=int)
+    parser.add_argument("-without_sent_pos", type=str2bool, nargs='?',const=False,default=False)
     
     
     parser.add_argument("-bert_data_path", default='')
