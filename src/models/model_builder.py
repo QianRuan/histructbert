d311e798bdd98d0499732e4fc0f9696b40c0123e
QianRuan <klararuan@gmail.com>
2021-06-01 14:41:01 +0200

    update

--- src/models/model_builder.py
+++ src/models/model_builder.py
@@ -277,11 +277,11 @@ class ExtSummarizer(nn.Module):
             self.bert.model.embeddings.position_embeddings = my_pos_embeddings
 #            print('#####self.bert.model.config.max_position_embeddings',self.bert.model.config.max_position_embeddings)
         
-        if(args.base_LM.startswith('longformer') and args.max_pos>4098):
+        if(args.base_LM.startswith('longformer') and args.max_pos>4096):
 #            print('2#####self.bert.model.config.max_position_embeddings',self.bert.model.config.max_position_embeddings)
             my_pos_embeddings = nn.Embedding(args.max_pos, self.bert.model.config.hidden_size)
-            my_pos_embeddings.weight.data[:4098] = self.bert.model.embeddings.position_embeddings.weight.data
-            my_pos_embeddings.weight.data[4098:] = self.bert.model.embeddings.position_embeddings.weight.data[-1][None,:].repeat(args.max_pos-4098,1)
+            my_pos_embeddings.weight.data[:4096] = self.bert.model.embeddings.position_embeddings.weight.data
+            my_pos_embeddings.weight.data[4096:] = self.bert.model.embeddings.position_embeddings.weight.data[-1][None,:].repeat(args.max_pos-4096,1)
             self.bert.model.embeddings.position_embeddings = my_pos_embeddings
 #            print('#####self.bert.model.config.max_position_embeddings',self.bert.model.config.max_position_embeddings)
         
