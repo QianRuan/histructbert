6b0c73aa6e39bd0980637e06a524bb708fc4c54f
QianRuan <klararuan@gmail.com>
2021-06-10 21:42:19 +0200

    upadate

--- src/models/model_builder.py
+++ src/models/model_builder.py
@@ -5,7 +5,7 @@ import torch.nn as nn
 from pytorch_transformers import BertModel, BertConfig
 from pytorch_transformers import RobertaModel
 from transformers import LongformerModel,LongformerConfig
-from transformers import PegasusTokenizer, BigBirdPegasusModel
+from transformers import PegasusTokenizer, BigBirdPegasusModel, BigBirdPegasusLearnedPositionalEmbedding
 from torch.nn.init import xavier_uniform_
 from others.logging import logger,init_logger
 from models.decoder import TransformerDecoder
@@ -308,7 +308,9 @@ class ExtSummarizer(nn.Module):
             
         if(args.base_LM.startswith('bigbird-pegasus') and args.max_pos>4096):
 #            print('2#####self.bert.model.config.max_position_embeddings',self.bert.model.config.max_position_embeddings)
-            my_pos_embeddings = nn.Embedding(args.max_pos, self.bert.model.config.hidden_size)
+#            print(type(self.bert.model.encoder.embed_positions),self.bert.model.encoder.embed_positions)
+            my_pos_embeddings = BigBirdPegasusLearnedPositionalEmbedding(args.max_pos, self.bert.model.config.hidden_size)
+            #my_pos_embeddings = nn.Embedding(args.max_pos, self.bert.model.config.hidden_size)
             my_pos_embeddings.weight.data[:4096] = self.bert.model.encoder.embed_positions.weight.data
             my_pos_embeddings.weight.data[4096:] = self.bert.model.encoder.embed_positions.weight.data[-1][None,:].repeat(args.max_pos-4096,1)
             self.bert.model.encoder.embed_positions = my_pos_embeddings
