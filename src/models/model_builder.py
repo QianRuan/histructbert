3f542b559bf83c8ce216ca48bf4447d427452c65
QianRuan <klararuan@gmail.com>
2021-06-19 05:31:07 +0200

    update

--- src/models/model_builder.py
+++ src/models/model_builder.py
@@ -157,8 +157,11 @@ class Roberta(nn.Module):
     def forward(self, x, segs, mask):
         if(self.finetune):
 #            top_vec, _ = self.model(x,  attention_mask=mask)
-            print('segs',segs)
+            print('segs',segs.shape,segs)
+            token_type_ids = torch.zeros(x.size(), dtype=torch.long)
+            print('token_type_ids',token_type_ids.shape,token_type_ids)
             print(self.model.embeddings.token_type_embeddings)
+            print(self.model.embeddings.token_type_embeddings(segs))
             top_vec = self.model(x, token_type_ids=segs, attention_mask=mask).last_hidden_state
         else:
             self.eval()
