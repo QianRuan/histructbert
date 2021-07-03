a25e5b51da061d59d6fc5dc1868c276c86a29ed5
QianRuan <klararuan@gmail.com>
2021-07-03 16:57:35 +0200

    update

--- src/prepro/pubmed_data_builder.py
+++ src/prepro/pubmed_data_builder.py
@@ -910,7 +910,7 @@ def encode_section_names_cls(args):
         sn_emb_dic_keys = list(torch.load(args.section_names_embed_path).keys())
         
     print(sn_emb_dic_values.shape,sn_emb_dic_values[0])
-    print(len(sn_emb_dic_keys),sn_emb_dic_keys[0])
+    print(len(sn_emb_dic_keys),sn_emb_dic_keys[:5])
     print(sn_cls_dic)
         
     assert 1==2
