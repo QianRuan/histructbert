3e6078f9854fb376b6d16db5a7a1fc9d9c7d114f
QianRuan <klararuan@gmail.com>
2021-07-05 15:30:17 +0200

    update

--- src/train.py
+++ src/train.py
@@ -30,7 +30,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("-task", default='ext', type=str, choices=['ext', 'abs'])
     parser.add_argument("-encoder", default='bert', type=str, choices=['bert', 'baseline'])
-    parser.add_argument("-mode", default='train', type=str, choices=['train', 'validate', 'test','test_steps','get_cand_list','lead','oracle'])
+    parser.add_argument("-mode", default='train', type=str, choices=['train', 'validate', 'test','test_steps','get_cand_list','lead','oracle',"test_steps_without_val"])
     parser.add_argument("-base_LM", default='bert-base', type=str, choices=['bert-base', 'bert-large', 'roberta-base','roberta-large','bart-base','bart-large','longformer-base-4096','longformer-large-4096','bigbird-pegasus-large-arxiv','bigbird-pegasus-large-pubmed'])
     parser.add_argument("-add_tok_struct_emb", type=str2bool, nargs='?',const=True,default=True)
     parser.add_argument("-add_sent_struct_emb", type=str2bool, nargs='?',const=True,default=True)
